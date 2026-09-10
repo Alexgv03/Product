4743c0022de8d5550e1274fa5398313f764ff128
Alexgv03 <alexisgv06@ciencias.unam.mx>
2026-09-10 10:35:05 -0600

    feat: implementacion de CtrlProduct y Category

--- src/main/java/com/product/controller/CtrlProduct.java
+++ src/main/java/com/product/controller/CtrlProduct.java
@@ -1,5 +1,11 @@
 package com.product.controller;
 
+/*
+/  Equipo:
+/         Gallardo Valdez Brayan Alexis
+/         Torres Miguel Emiliano        
+*/
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,30 +15,30 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.product.entity.Category;
 
-// Indicamos que es un controlador REST y la ruta principal
+// decimos que es un controlador REST y la ruta principal
 @RestController
 @RequestMapping("/category") 
 public class CtrlProduct {
 
-    // Maneja las peticiones GET a /category
+    // maneja las peticiones GET a /category
     @GetMapping
     public List<Category> getCategories() {
         
-        // Creamos la lista en memoria (tiempo de ejecución)
+        // se crea la lista en tiempo de ejecucion
         List<Category> categories = new ArrayList<>();
         
-        // Primera categoría padre
+        // 1ra categoria padre
         Category ropa = new Category("Ropa", "RP", null);
         ropa.setCategory_id(1); // Forzamos el ID para que coincida con el ejemplo
         
-        // Segunda categoría (hija de ropa)
+        // 2da categoría (hija de ropa)
         Category calzado = new Category("Calzado", "CLZD", 1);
         calzado.setCategory_id(2);
         
         categories.add(ropa);
         categories.add(calzado);
         
-        // Spring Web convertirá automáticamente esta lista a JSON (Jackson)
+        // Spring Web convierte esta lista a json
         return categories; 
     }
 }
