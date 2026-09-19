c7ebf487cabc633b7fc1a38043c3a0d5c83eadcf
Alexgv03 <alexisgv06@ciencias.unam.mx>
2026-09-19 12:58:19 -0600

    feat: implementacion de manejo de excepciones y ResponseEntity

--- src/main/java/com/product/api/controller/CtrlCategory.java
+++ src/main/java/com/product/api/controller/CtrlCategory.java
@@ -1,13 +1,8 @@
-/*
- Equipo: 
-        Gallardo Valdez Brayan Alexis
-        Torres Miguel Emiliano
-*/
-
 package com.product.api.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.product.api.entity.Category;
 import com.product.api.service.SvcCategory;
 
-@RestController //es controlador
-@RequestMapping("/category") //ruta base para los metodos de esta clase
+@RestController
+@RequestMapping("/category")
 public class CtrlCategory {
 
     final SvcCategory svc;
@@ -25,10 +20,8 @@ public class CtrlCategory {
         this.svc = svc;
     }
 
-    // mapea solicitud http get
     @GetMapping
-    public List<Category> getCategories() {
-        // regresa la lista con las categorias desde la bd
+    public ResponseEntity<List<Category>> getCategories() {
         return svc.getCategories();
     }
 }
--- src/main/java/com/product/api/service/SvcCategory.java
+++ src/main/java/com/product/api/service/SvcCategory.java
@@ -1,16 +1,12 @@
-/*
- Equipo: 
-        Gallardo Valdez Brayan Alexis
-        Torres Miguel Emiliano
-*/
-
 package com.product.api.service;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+
 import com.product.api.entity.Category;
 
 public interface SvcCategory {
-    List<Category> getCategories();
-    List<Category> getActiveCategories();
+    ResponseEntity<List<Category>> getCategories();
+    ResponseEntity<List<Category>> getActiveCategories();
 }
